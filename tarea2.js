38d6cd176c9ef84dd87ef9c2cb03d0f8d10b9ba3
Gabriel Acosta <gabriacos@gmail.coma>
2023-04-28 15:16:48 -0300

    entrega de segunda tarea

---
+++ tarea2.js
@@ -0,0 +1,15 @@
+const name = "Acosta Gabriel";
+const edad = 24;
+
+const Desarrollador = true;
+
+const fechaNac = new Date(1998, 8, 29);
+
+const libro = {
+    "nombre": "python desde 0",
+    "hojas": 250,
+
+
+}
+const Lista = [ name, edad, Desarrollador, fechaNac, libro
+];
